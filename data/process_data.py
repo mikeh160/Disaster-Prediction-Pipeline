9c69a30de087375461d616da30f3d26a8df98de5
mikeh160 <mubashirh160@gmail.com>
2021-07-15 00:37:54 +0500

    fixed the indentation errors by making all indents with space

--- data/process_data.py
+++ data/process_data.py
@@ -6,13 +6,14 @@ import sqlite3
 
 def load_data(messages_filepath, categories_filepath):
 
-'''Reads the data from the given filepaths and merges them into a single dataframe(df)
-
-    *inputs : 
+    '''
+    Reads the data from the given filepaths and merges them into a single dataframe(df)
+    
+        inputs:
             csv file paths
-    *returns: 
-        dataframe(df)
-'''
+        returns:
+            dataframe(df)
+    '''
     
     messages = pd.read_csv(str(messages_filepath))
     categories = pd.read_csv(str(categories_filepath))
@@ -24,14 +25,14 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-'''
-Takes the raw df , manipulates and cleans the data by spreading out results under single column (categories) into multiple columns, removing unnecessary characters, redefining data types of new columns and dropping the non-required columns. Further concatenates the results and drops duplicates
 
-    *inputs : 
+    ''' Takes the raw data df, manipulates the data shape into desired SQL table, cleans and saves the result  
+    
+        *inputs:
             df
-    *returns: 
+        *returns:
             df
-'''
+    '''
     
     categories = df.categories.str.split(pat=';', n=-1, expand = True)
     row = categories.loc[0,:]
@@ -43,7 +44,7 @@ Takes the raw df , manipulates and cleans the data by spreading out results unde
         categories[column] = categories[column].str.extract('.*(\d{1})', expand = False)
         # convert column from string to numeric
         categories[column] = categories[column].astype(int)
-    
+        
     df.drop('categories', 1, inplace = True)
     df = pd.concat([df, categories], join = 'inner', sort = False, axis = 1)
     df.groupby(df.columns.tolist(), as_index = False).size().reset_index().rename(columns = {0:"duplicates"})
@@ -55,38 +56,37 @@ Takes the raw df , manipulates and cleans the data by spreading out results unde
 
 def save_data(df, database_filename):
 
-'''
-Takes a dataframe(df) and filepath and saves the result of that dataframe(df) into an sql table
-
-    *inputs : 
+    '''
+    Takes a dataframe(df) and filepath and saves the result of that dataframe(df) into an sql table
+    
+        *inputs:
             df, df_filepath
-    *returns: 
-        saves an SQL table
-'''
+        *returns:
+            saves an SQL table
+    '''
 
     engine = create_engine(f'sqlite:///{database_filename}')
-    df.to_sql('disaster_response_local_name', engine, index=False)
+    df.to_sql('disaster_response_local_name', engine, if_exists = 'replace' , index=False)
     engine.dispose()
     #pass  
 
 
 def main():
     
-'''
-Specifying the program input in the format of messages_filepath, categories_filepath, db_filepath in command terminal runs the main program
-
-    *inputs : 
+    '''
+    Specifying the program input in message_filepath, categories_filepath & db_filepath to run the main program
+    
+        *inputs:
             user sppecified input specifying messages_filepath, categories_filepath and database_filepath in terminal
-    *returns: 
-        runs the entire process_data.py modules
-'''
+        *returns:
+            runs the entire process_data.py modules
+    '''
     
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'.format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
@@ -96,7 +96,7 @@ Specifying the program input in the format of messages_filepath, categories_file
         save_data(df, database_filepath)
         
         print('Cleaned data saved to database!')
-    
+        
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
